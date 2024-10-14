6f3f9bd16c17fec563d3febbe128d49920ad9c57
Meet2407 <meetkapadiya65@gmail.com>
2024-10-14 10:19:45 +0530

    Initial Commit

---
+++ Exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam
+//
+//  Created by R90 on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
