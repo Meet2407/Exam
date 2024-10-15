8a1643e7fc782a382992ebfc95bfb9cfb24b5d34
Meet2407 <meetkapadiya65@gmail.com>
2024-10-15 10:35:33 +0530

    update the code

---
+++ Exam/displayTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  displayTableViewCell.swift
+//  Exam
+//
+//  Created by R90 on 14/10/24.
+//
+
+import UIKit
+
+class displayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
